ea969f81dbf9fdf92fc9371a766db48d31f313e7
aerain <j40f893@gmail.com>
2020-01-03 10:28:01 +0900

    test

--- src/index.js
+++ src/index.js
@@ -292,12 +292,12 @@ export default class extends Component {
     // related to https://github.com/leecade/react-native-swiper/issues/570
     // contentOffset is not working in react 0.48.x so we need to use scrollTo
     // to emulate offset.
-    if (Platform.OS === 'ios') {
-      if (this.initialRender && this.state.total > 1) {
-        this.scrollView.scrollTo({...offset, animated: false})
-        this.initialRender = false;
-      }
-    }
+    // if (Platform.OS === 'ios') {
+    //   if (this.initialRender && this.state.total > 1) {
+    //     this.scrollView.scrollTo({...offset, animated: false})
+    //     this.initialRender = false;
+    //   }
+    // }
 
     this.setState(state)
   }
@@ -465,9 +465,10 @@ export default class extends Component {
 
     if (Platform.OS !== 'ios') {
       this.scrollView && this.scrollView[animated ? 'setPage' : 'setPageWithoutAnimation'](diff)
-    } else {
-      this.scrollView && this.scrollView.scrollTo({ x, y, animated })
-    }
+    } 
+    // else {
+    //   this.scrollView && this.scrollView.scrollTo({ x, y, animated })
+    // }
 
     // update scroll state
     this.internals.isScrolling = true
